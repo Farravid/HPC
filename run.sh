579d41ee379f509ce058868efaf81fbe8545b776
David Martínez García <davidmg103@hotmail.com>
2024-07-07 13:04:29 +0200

    Moving HPC to Linux WSL because linux is faster overall and more comfy

---
+++ run.sh
@@ -0,0 +1,7 @@
+rm -rf build
+
+conan install . --output-folder=build --build=missing
+cd build
+cmake .. -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE="conan_toolchain.cmake" -DCMAKE_BUILD_TYPE="Release"
+cmake --build .
+./learning-hpc
